3934c43ef7f2d2c9a884d88cd27272404c8a379c
Jeroen Bakker <jeroen@blender.org>
2020-09-10 08:11:33 +0200

    Snap vertices to pixel grid

--- source/blender/draw/engines/overlay/shaders/edit_uv_edges_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/edit_uv_edges_vert.glsl
@@ -13,6 +13,11 @@ void main()
 {
   vec3 world_pos = point_object_to_world(vec3(u, 0.0));
   gl_Position = point_world_to_ndc(world_pos);
+  /* Snap vertices to the pixel grid to reduce artifacts. */
+  vec2 half_viewport_res = sizeViewport.xy * 0.5;
+  vec2 half_pixel_offset = sizeViewportInv * 0.5;
+  gl_Position.xy = round(gl_Position.xy * half_viewport_res) / half_viewport_res +
+                   half_pixel_offset;
 
   bool is_select = (flag & VERT_UV_SELECT) != 0;
   if (is_select) {
